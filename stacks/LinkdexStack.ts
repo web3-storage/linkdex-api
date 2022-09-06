de0f975336d69727a3826e05d74e358bdde35ede
Oli Evans <oli@protocol.ai>
2022-09-06 18:04:28 +0100

    chore: pass bucketName as env
    also fix lambda config
    - no timeout. run till you can run no more
    - use arm for cheaper
    
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- stacks/LinkdexStack.ts
+++ stacks/LinkdexStack.ts
@@ -1,13 +1,16 @@
 import { StackContext, Api, Bucket } from "@serverless-stack/resources";
 
-export function LinkdexStack({ stack }: StackContext) {
+export function LinkdexStack({ app,stack }: StackContext) {
 
   const bucket = new Bucket(stack, 'cars')
 
   const api = new Api(stack, "api", {
     defaults: {
       function: {
-        permissions: [bucket]
+        permissions: [bucket],
+        environment: { BUCKET_NAME: bucket.bucketName },
+        architecture: 'arm_64', // cheaper, so why not?
+        timeout: 0 // default is 10s
       }
     },
     routes: {
