a782db2b5548a70f8accd0b5f76719e464a365a2
Oli Evans <oli@protocol.ai>
2022-09-07 21:45:36 +0100

    feat: use custom domain linkdex.dag.haus
    prod lives at `linkdex.dag.haus`
    staging is at `staging.linkdex.dag.haus`
    
    ...all non-prod stages follow that pattern `${stage}.linkdex.dag.haus`
    
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- stacks/LinkdexStack.ts
+++ stacks/LinkdexStack.ts
@@ -1,14 +1,21 @@
 import { StackContext, Api, Bucket } from "@serverless-stack/resources"
 import { aws_s3 as s3 } from 'aws-cdk-lib'
 
-export function LinkdexStack({ app,stack }: StackContext) {
+export function LinkdexStack({ app, stack }: StackContext) {
   // either import an existing bucket or create a new one for dev.
   // see: https://docs.sst.dev/advanced/importing-resources
   const bucket = (process.env.BUCKET_NAME)
     ? new Bucket(stack, 'existing-cars', { cdk: { bucket: s3.Bucket.fromBucketName(stack, 'imported-cars', process.env.BUCKET_NAME) }})
     : new Bucket(stack, 'cars')
 
+  const zone = 'linkdex.dag.haus'
+  const domain = app.stage === 'prod' ? zone : `${app.stage}.zone`
+
   const api = new Api(stack, "api", {
+    customDomain: { 
+      domainName: domain,
+      hostedZone: zone
+    },
     defaults: {
       function: {
         permissions: [bucket],
