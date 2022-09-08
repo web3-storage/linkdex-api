4af8a3ff87d1d762023711c9bd9dc50492027eab
Oli Evans <oli@protocol.ai>
2022-09-08 16:45:10 +0100

    !feat: support identity CIDs
    Updates to `linkdex@2.0.0` to bring in support for reporting on DAGs that contain identity CIDs
    
    see: https://github.com/web3-storage/linkdex/pull/10
    
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- services/test/linkdex.test.js
+++ services/test/linkdex.test.js
@@ -1,8 +1,8 @@
 import test from 'ava'
-import { Blob } from 'buffer'
 import * as pb from '@ipld/dag-pb'
 import { customAlphabet } from 'nanoid'
 import { encode } from 'multiformats/block'
+import { identity } from 'multiformats/hashes/identity'
 import { CarBufferWriter } from '@ipld/car'
 import { GenericContainer } from 'testcontainers'
 import { sha256 as hasher } from 'multiformats/hashes/sha2'
@@ -33,8 +33,8 @@ test('ignores non CAR files', async t => {
     cars: [ key ],
     structure: 'Complete',
     blocksIndexed: 3,
-    blocksUnique: 3,
-    blocksUndecodeable: 0
+    uniqueCids: 3,
+    undecodeable: 0
   })
 })
 
@@ -58,8 +58,51 @@ test('complete CAR', async t => {
     cars: [ key ],
     structure: 'Complete',
     blocksIndexed: 3,
-    blocksUnique: 3,
-    blocksUndecodeable: 0
+    uniqueCids: 3,
+    undecodeable: 0
+  })
+})
+
+test('complete CAR with identity CIDs as links', async t => {
+  const { s3 } = t.context
+  const bucket = await createBucket(s3)
+  const id = await encode({ value: pb.prepare({ Data: 'itsa me! mario!' }), codec: pb, hasher: identity })
+  const parent = await encode({ value: pb.prepare({ Links: [id.cid] }), codec: pb, hasher })
+  const car = CarBufferWriter.createWriter(Buffer.alloc(1000), { roots: [parent.cid]})
+  car.write(parent)
+  const key = `raw/${parent.cid.toString()}/user/complete.car`
+  await s3.send(new PutObjectCommand({ Bucket: bucket, Key: key, Body: car.close() }))
+  const res = await getLinkdexReport(key, bucket, s3)
+  t.is(res.statusCode, 200)
+  const report = JSON.parse(res.body)
+  t.deepEqual(report, {
+    cars: [ key ],
+    structure: 'Complete',
+    blocksIndexed: 1,
+    uniqueCids: 2,
+    undecodeable: 0
+  })
+})
+
+test('complete CAR with identity CIDs as links and in CAR', async t => {
+  const { s3 } = t.context
+  const bucket = await createBucket(s3)
+  const id = await encode({ value: pb.prepare({ Data: 'itsa me! mario!' }), codec: pb, hasher: identity })
+  const parent = await encode({ value: pb.prepare({ Links: [id.cid] }), codec: pb, hasher })
+  const car = CarBufferWriter.createWriter(Buffer.alloc(1000), { roots: [parent.cid]})
+  car.write(parent)
+  car.write(id)
+  const key = `raw/${parent.cid.toString()}/user/complete.car`
+  await s3.send(new PutObjectCommand({ Bucket: bucket, Key: key, Body: car.close() }))
+  const res = await getLinkdexReport(key, bucket, s3)
+  t.is(res.statusCode, 200)
+  const report = JSON.parse(res.body)
+  t.deepEqual(report, {
+    cars: [ key ],
+    structure: 'Complete',
+    blocksIndexed: 2,
+    uniqueCids: 2,
+    undecodeable: 0
   })
 })
 
@@ -87,8 +130,8 @@ test('complete across 2 CARs', async t => {
     cars: [ key1, key2 ],
     structure: 'Complete',
     blocksIndexed: 4,
-    blocksUnique: 3,
-    blocksUndecodeable: 0
+    uniqueCids: 3,
+    undecodeable: 0
   })
 })
 
@@ -117,8 +160,8 @@ test('incomplete across 2 CARs', async t => {
     cars: [ key1, key2 ],
     structure: 'Partial',
     blocksIndexed: 4,
-    blocksUnique: 3,
-    blocksUndecodeable: 0
+    uniqueCids: 3,
+    undecodeable: 0
   })
 })
 
