6779a8c9756fd4d2aa0268fe96fcefd32b50b9d4
Oli Evans <oli@protocol.ai>
2022-10-11 15:09:16 +0100

    feat: add aws resource tags
    tags let us discover all the aws resource costs incurred by this app.
    see: https://docs.sst.dev/advanced/tagging-resources
    
    existing tag names de-facto decided in:
    - https://github.com/elastic-ipfs/infrastructure/blob/9d64210b0c4a0872a7bb9dda0a5a789524790b08/terraspace/config/terraform/provider.tf
    - https://github.com/web3-storage/w3up/pull/192/files#diff-5817f07e292e15f736afda451ac2afa6642143cc58bbca545a736322042ef744R13-R18
    
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,3 +1,4 @@
+import { Tags } from "aws-cdk-lib";
 import { LinkdexStack } from "./LinkdexStack";
 import { App } from "@serverless-stack/resources";
 
@@ -10,4 +11,11 @@ export default function (app: App) {
     },
   });
   app.stack(LinkdexStack);
+  
+  // tags let us discover all the aws resource costs incurred by this app
+  // see: https://docs.sst.dev/advanced/tagging-resources
+  Tags.of(app).add('Project', 'linkdex-api')
+  Tags.of(app).add('Repository', 'https://github.com/web3-storage/linkdex-api')
+  Tags.of(app).add("Environment", `${app.stage}-${app.region}`)
+  Tags.of(app).add('ManagedBy', 'SST')
 }
