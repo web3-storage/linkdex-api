19fc5328b6e83c8add02f3426b5f4a306d715e05
Alan Shaw <alan.shaw@protocol.ai>
2023-05-11 14:18:17 +0100

    feat: pass indexer to reporter
    Co-authored-by: Oli Evans <oli@protocol.ai>

--- services/functions/linkdex-cid.ts
+++ services/functions/linkdex-cid.ts
@@ -29,7 +29,8 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   const hash = event.rawQueryString.includes('hash=true')
   const bucket = process.env.BUCKET_NAME ?? ''
   const s3 = new S3Client({})
-  const reporters = [new CompleteReporter(bucket, s3, { hash }), new RawReporter(bucket, s3, { hash })]
+  const index = this.options.hash ? new HashingLinkIndexer() : new LinkIndexer()
+  const reporters = [new CompleteReporter(bucket, s3, index), new RawReporter(bucket, s3, index)]
   try {
     return await getLinkdexReportForCid(cid, reporters)
   } catch (err: any) {
